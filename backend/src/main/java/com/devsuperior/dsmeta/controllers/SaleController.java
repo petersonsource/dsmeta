84c51e4d6e1444b3dbbb0f67fb40e812829b4f9c
Peterson dos Santos <peterson.opensource@hotmail.com>
2022-11-16 13:10:23 -0300

    Date Select

--- backend/src/main/java/com/devsuperior/dsmeta/controllers/SaleController.java
+++ backend/src/main/java/com/devsuperior/dsmeta/controllers/SaleController.java
@@ -3,8 +3,11 @@ package com.devsuperior.dsmeta.controllers;
 import java.util.List;
 
 import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.data.domain.Page;
+import org.springframework.data.domain.Pageable;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RequestParam;
 import org.springframework.web.bind.annotation.RestController;
 
 import com.devsuperior.dsmeta.entities.Sale;
@@ -18,8 +21,11 @@ public class SaleController {
 	private SalesService salesService;
 	
 	@GetMapping
-	public List<Sale> findSales(){
-		return salesService.findSales();
+	public Page<Sale> findSales(
+			@RequestParam(value = "minDate", defaultValue = "") String minDate,
+			@RequestParam(value = "maxDate", defaultValue = "") String maxDate,
+			Pageable pageable){
+		return salesService.findSales(minDate, maxDate, pageable);
 	}
 	
 
