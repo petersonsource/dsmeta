6167009356a42f6f387710afeda9b183e41844d0
Peterson dos Santos <peterson.opensource@hotmail.com>
2022-11-18 19:49:42 -0300

    fix tela com buscas vazias

--- frontend/src/components/SalesCard/index.tsx
+++ frontend/src/components/SalesCard/index.tsx
@@ -21,9 +21,16 @@ function SalesCard() {
 
     useEffect(() => {
         
-        const dmin =  minDate.toISOString().slice(0, 10);
-        const dmax =  maxDate.toISOString().slice(0, 10);
-      
+        var dmin;
+        var dmax ;
+
+        if (minDate !== null && maxDate !==null) {
+
+             dmin =  minDate.toISOString().slice(0, 10);
+             dmax =  maxDate.toISOString().slice(0, 10);
+        
+        }
+
         axios.get(`${BASE_URL}/sales?minDate=${dmin}&maxDate=${dmax}`)
             .then(response => {
                 setSales(response.data.content)
